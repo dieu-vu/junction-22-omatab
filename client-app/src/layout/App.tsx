d660a3dab30aca71f91d7a30df0a834e514a23fc
Huy Mai <me@huymai.fi>
2022-11-05 11:26:47 +0200

    Add eslint ignore for unused variable

--- client-app/src/layout/App.tsx
+++ client-app/src/layout/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { ReactElement } from 'react';
 import OrderSummary from '../features/orderSummary/OrderSummary';
 import {
@@ -9,6 +10,11 @@ import {
 } from 'react-router-dom';
 import PickupLocation from '../features/deliveryInfo/PickupLocation';
 import DropoffLocation from '../features/deliveryInfo/DropoffLocation';
+=======
+//TODO: Check if this element is needed later
+import React from 'react'; // eslint-disable-line no-unused-vars
+import Header from '../components/Header/Header';
+>>>>>>> Add eslint ignore for unused variable
 
 const router = createBrowserRouter(
   createRoutesFromElements(
