3f5565596d7a9d2def7af5a08216ab9300b10197
GiangNguyen1207 <nguyengiang.nchg@gmail.com>
2022-11-05 20:01:02 +0200

    remove unnecessary React

--- client-app/src/hooks/apiHook.ts
+++ client-app/src/hooks/apiHook.ts
@@ -1,43 +1,46 @@
-import React from 'react';
 import { OrderDTO } from './OrderDTO';
 
-
-const baseUrl: string = 'https://daas-public-api.development.dev.woltapi.com/merchants/';
-const api_key: string = process.env.REACT_APP_API_KEY || ''; 
+const baseUrl: string =
+  'https://daas-public-api.development.dev.woltapi.com/merchants/';
+const api_key: string = process.env.REACT_APP_API_KEY || '';
 const merchant_id: string = process.env.REACT_APP_MERCHANT_ID || '';
 
-console.log("env var: ", process.env.REACT_APP_MERCHANT_ID || 'env var not found');
+console.log(
+  'env var: ',
+  process.env.REACT_APP_MERCHANT_ID || 'env var not found'
+);
 
 const postOrder = async (data: OrderDTO) => {
-  console.log("post data object", JSON.stringify(data));
-    const options = {
-      method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${api_key}`,
-          'Access-Control-Allow-Origin': '*',
-          "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-        },
-        body: JSON.stringify(data),
-    };
-    try {
-    const response =  await fetch(`${baseUrl}${merchant_id}/delivery-order`, options);
+  console.log('post data object', JSON.stringify(data));
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${api_key}`,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
+    },
+    body: JSON.stringify(data),
+  };
+  try {
+    const response = await fetch(
+      `${baseUrl}${merchant_id}/delivery-order`,
+      options
+    );
     const json = await response.json();
-      if (response.ok) {
-        return json;
-      } else {
-        throw new Error(
-          json.error
-            ? `${json.message} : ${json.error}`
-            : json.message || response.statusText
-        );
-      }
-    } catch (e) {
-      throw new Error('error in post request');
+    if (response.ok) {
+      return json;
+    } else {
+      throw new Error(
+        json.error
+          ? `${json.message} : ${json.error}`
+          : json.message || response.statusText
+      );
     }
-}
-
+  } catch (e) {
+    throw new Error('error in post request');
+  }
+};
 
 export { postOrder };
-
-
