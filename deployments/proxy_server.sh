9f222057735ab51c50740f925f9ec9356af1253c
Huy Mai <me@huymai.fi>
2022-11-05 21:45:12 +0200

    Fix proxy policy

--- deployments/proxy_server.sh
+++ deployments/proxy_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 . /home/${USER}/.nvm/nvm.sh
-cd /home/${USER}/proxy_server && npm start > /home/${USER}/proxy.log
+cd /home/${USER}/proxy_server && npm start >> /home/${USER}/proxy.log &2>1
--- proxy_server/index.js
+++ proxy_server/index.js
@@ -17,9 +17,8 @@ app.use(cors())
 app.use('', (req, res, next) => {
     const origin = req.headers.origin;
     console.log(`Received request from ${origin}`);
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
     return next();
