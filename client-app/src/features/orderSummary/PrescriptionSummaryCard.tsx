991fbc0c382f4c168bd106fc0af8e496d4bc8d82
Huy Mai <me@huymai.fi>
2022-11-05 13:02:18 +0200

    Test server auto reload

--- client-app/src/features/orderSummary/PrescriptionSummaryCard.tsx
+++ client-app/src/features/orderSummary/PrescriptionSummaryCard.tsx
@@ -43,7 +43,7 @@ export default function PrescriptionSummary() {
         <Typography variant='body2' color='text.secondary'>
           This impressive paella is a perfect party dish and a fun meal to cook
           together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          mussels, if you like to die.
         </Typography>
         <CardActions sx={{ marginY: 2 }}>
           <ExpandMore
