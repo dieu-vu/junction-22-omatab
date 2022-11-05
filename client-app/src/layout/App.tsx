e896b70a42d4e34994e1dc97a5b4d4679908e4c1
GiangNguyen1207 <nguyengiang.nchg@gmail.com>
2022-11-05 10:34:35 +0200

    add header component

--- client-app/src/layout/App.tsx
+++ client-app/src/layout/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Header from '../components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="Omatab">
-        <p>
-          Omatab
-        </p>
+    <div className='App'>
+      <header className='Omatab'>
+        <Header title='Delivery Information' />
       </header>
     </div>
   );
